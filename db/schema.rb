8b68ac7e48f48a4403501fb04e4c8f453852edda
BunyakornGoko <bunyakorn.porn@mail.kmutt.ac.th>
2025-03-06 15:30:51 +0700

    Add user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_03_06_075600) do
+ActiveRecord::Schema[8.0].define(version: 2025_03_06_082257) do
   create_table "products", force: :cascade do |t|
     t.string "name", null: false
     t.text "description"
@@ -18,5 +18,15 @@ ActiveRecord::Schema[8.0].define(version: 2025_03_06_075600) do
     t.integer "stock_quantity", default: 0, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "first_name", null: false
+    t.string "last_name", null: false
+    t.string "email", null: false
+    t.string "role", default: "customer", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 end
