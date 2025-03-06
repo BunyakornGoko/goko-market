afeaab78f1da2eec893f6e753eaf546bd511029b
BunyakornGoko <bunyakorn.porn@mail.kmutt.ac.th>
2025-03-06 14:47:50 +0700

    Add daisyui

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,13 @@
+class PagesController < ApplicationController
+  def home
+    # Render the home page
+  end
+
+  def about
+    # Render the about page
+  end
+
+  def contact
+    # Render the contact page
+  end
+end 
